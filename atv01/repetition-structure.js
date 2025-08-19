9f6f082c0771cfbc05e0fda3497a0303e673caee
vctrr1 <victoremaanoel@outlook.com>
2025-08-19 12:27:22 -0300

    add while to file

--- atv01/repetition-structure.js
+++ atv01/repetition-structure.js
@@ -1,6 +1,13 @@
 let arr = [];
 
 for (let i = 0; i < 50; i++) {
-  arr[i] = (Math.random() * 100).toFixed(0);
-  console.log(arr[i]);
+  arr[i] = Math.random() * 101;
+  console.log("array Original: ", arr[i].toFixed(0));
+}
+
+let j = 0;
+while (j < arr.length) {
+  arr[j] = arr[j] * 2;
+  console.log("array multiplicado: ", arr[j].toFixed(0));
+  j++;
 }
