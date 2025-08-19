260fc3ae1b2ca5a12cafdfdf5d1302e40296061c
vctrr1 <victoremaanoel@outlook.com>
2025-08-19 11:58:29 -0300

    add conditional structure

---
+++ atv01/conditional-structure.js
@@ -0,0 +1,9 @@
+let idade = 70;
+
+if (idade < 16) {
+  console.log("Proibido votar.");
+} else if (idade >= 18 && idade < 70) {
+  console.log("Obrigado votar.");
+} else {
+  console.log("Opcional votar.");
+}
